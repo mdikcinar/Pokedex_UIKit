8950da2545d5f27c7e6f25e23d1bc08a306ac7fe
Mustafa Ali Dikçinar <mustafaalidikcinar@hotmail.com>
2023-06-30 22:22:05 +0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Mustafa Ali Dikcinar on 30.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
